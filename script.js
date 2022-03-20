034f54ddbb8c655cd55565b57420aabb496b42d8
boris <boris@gratchev.com>
2022-03-20 13:00:03 +0100

    added batman image as clickable link

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ function loadProgram(){
     var div1 = document.getElementById("div1")
 
     var movie1 = document.createElement("p")
+
     var text = document.createTextNode("Movie 1")
+
+    var link = document.createElement("a")
+    link.href = "https://www.cineplex.de/film/the-batman/371837/mannheim/"
+
+    var image = document.createElement("img")
+    image.src = "batman.jpg"
+    image.style = "width: 20%;"
+    image.href = "www.google.com"
+
 
     movie1.appendChild(text)
 
+    link.appendChild(image)
+    movie1.appendChild(link)
     div1.appendChild(movie1)
 
 }
